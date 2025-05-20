3b004e5546883a9ed96bb35f7df55c5c51830687
Isaías Oliveira <isaiaswebnet@gmail.com>
2025-05-19 22:04:37 -0300

    produtos
    produtos

--- src/server.js
+++ src/server.js
@@ -6,16 +6,21 @@ const server = http.createServer((req, res) => {
     if (method === 'GET' && url === '/produtos') {
         return res.end(
             'ID: 1, Nome: Cerveja Heineken\n' +
-            'ID: 2, Nome: Coca-Cola'
+            'ID: 2, Nome: Coca-Cola\n' +
+            'ID: 3, Nome: Água Mineral'
         );
     }
 
-    if (method === 'GET' && url === '/produto1') {
+    if (method === 'POST' && url === '/produto1') {
         return res.end('ID: 1, Nome: Cerveja Heineken');
     }
 
-    if (method === 'GET' && url === '/produto2') {
+    if (method === 'POST' && url === '/produto2') {
         return res.end('ID: 2, Nome: Coca-Cola');
+    }
+
+    if (method === 'POST' && url === '/produto3') {
+        return res.end('ID: 3, Nome: Água Mineral');
     }
 
     return res.end('Página inicial');
